a7816d140da0d67281e53145c4f713ef9bb4af87
Pavel Ivanov <splinter928@gmail.com>
2022-03-31 22:31:13 +0300

    2nd commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+#    image = models.ImageField(upload_to='blog/images/')
+#    url = models.URLField(blank=True)   #We used 'blank=True' to open url in new tab
+    date = models.DateField()
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Project
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, r'portfolio\home.html', {'projects': projects})
+#    return HttpResponse('Hello')
